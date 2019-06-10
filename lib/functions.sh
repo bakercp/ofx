24568ee49ab64617f98483e8974cf3821654b1a4
Christopher Baker <me@christopherbaker.net>
2019-06-10 17:25:20 -0600

    Update ofx.

--- lib/functions.sh
+++ lib/functions.sh
@@ -51,15 +51,6 @@ function os()
 function relpath()
 {
   echo $(perl -e 'use File::Spec; print File::Spec->abs2rel(@ARGV) . "\n"' ${1} ${2})
-
-  # b=""
-  # s=$(cd ${1%%/}; pwd); 
-  # d=$(cd $2;pwd);
-  # while [ "${d#$s/}" == "${d}" ]
-  # do s=$(dirname $s);
-  # b="../${b}"; 
-  # done; 
-  # echo ${b}${d#$s/}
 }
 
 function get_max_number_of_jobs()
