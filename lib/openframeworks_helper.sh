c59b2ad364be2239f8c3f22cba2235dd7714981e
Christopher Baker <me@christopherbaker.net>
2019-06-09 22:38:20 -0600

    Don't exit.

--- lib/openframeworks_helper.sh
+++ lib/openframeworks_helper.sh
@@ -8,7 +8,6 @@ function install_openframeworks()
     pushd $1 > /dev/null
     git clone --depth=${OF_CLONE_DEPTH} --branch=${OF_CLONE_BRANCH} https://github.com/${OF_CLONE_USERNAME}/openFrameworks
     popd > /dev/null
-    exit 0
   else 
     echoError "openFrameworks install directory does not exist: \"$1\""
     exit 1
