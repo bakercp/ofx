d9f350ed983b77e1c19d176bf60cb366e09f5a15
Christopher Baker <me@christopherbaker.net>
2019-06-10 17:49:02 -0600

    Update ofx.

--- lib/openframeworks_helper.sh
+++ lib/openframeworks_helper.sh
@@ -5,13 +5,18 @@
 function install_openframeworks()
 {
     echoFancy "Installing" "openFrameworks"
-    if [ ! -d ${1} ]; then
-        git clone --depth=${OF_CLONE_DEPTH} --branch=${OF_CLONE_BRANCH} https://github.com/${OF_CLONE_USERNAME}/openFrameworks.git ${1}
-        pushd ${1} > /dev/null
+    if [ ! -d ${OF_ROOT} ]; then
+        git clone --depth=${OF_CLONE_DEPTH} --branch=${OF_CLONE_BRANCH} https://github.com/${OF_CLONE_USERNAME}/openFrameworks.git ${OF_ROOT}
+        pushd ${OF_ROOT} > /dev/null
         scripts/ci/addons/install.sh
+
+        # Undo this https://github.com/openframeworks/openFrameworks/blob/master/scripts/ci/addons/install.sh#L39 
+        # because we use a symlink.
+        mv ${OF_ADDON_PATH} ${TRAVIS_BUILD_DIR}
+
         popd > /dev/null
     else 
-        echoWarning "${1} already exists"
+        echoWarning "${OF_ROOT} already exists"
     fi
 }
 
