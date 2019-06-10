9bda2f508ae2cd5ccad1b7133a41d62203b3ee57
Christopher Baker <me@christopherbaker.net>
2019-06-10 15:53:47 -0600

    Add CI.

--- lib/openframeworks_helper.sh
+++ lib/openframeworks_helper.sh
@@ -4,6 +4,7 @@
 
 function install_openframeworks()
 {
+    echoFancy "Installing" "openFrameworks"
     git clone --depth=${OF_CLONE_DEPTH} --branch=${OF_CLONE_BRANCH} https://github.com/${OF_CLONE_USERNAME}/openFrameworks.git ${OF_ROOT}
 }
 
