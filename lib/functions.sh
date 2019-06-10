04b8232e18874d842b33de708987fad0d97e9ee7
Christopher Baker <me@christopherbaker.net>
2019-06-10 16:26:27 -0600

    Updates.

--- lib/functions.sh
+++ lib/functions.sh
@@ -47,6 +47,65 @@ function os()
   return 0
 }
 
+
+function relpath()
+{
+  b=""
+  s=$(cd ${1%%/}; pwd); 
+  d=$(cd $2;pwd);
+  while [ "${d#$s/}" == "${d}" ]
+  do s=$(dirname $s);
+  b="../${b}"; 
+  done; 
+  echo ${b}${d#$s/}
+
+  # # both $1 and $2 are absolute paths beginning with /
+  # # returns relative path to $2/$target from $1/$source
+  # source=(readlink -f $1)
+  # target=(readlink -f $2)
+
+  # common_part=$source # for now
+  # result="" # for now
+
+  # while [[ "${target#$common_part}" == "${target}" ]]; do
+  #     # no match, means that candidate common part is not correct
+  #     # go up one level (reduce common part)
+  #     common_part="$(dirname $common_part)"
+  #     # and record that we went back, with correct / handling
+  #     if [[ -z $result ]]; then
+  #         result=".."
+  #     else
+  #         result="../$result"
+  #     fi
+  # done
+
+  # if [[ $common_part == "/" ]]; then
+  #     # special case for root (no common path)
+  #     result="$result/"
+  # fi
+
+  # # since we now have identified the common part,
+  # # compute the non-common part
+  # forward_part="${target#$common_part}"
+
+  # # and now stick all parts together
+  # if [[ -n $result ]] && [[ -n $forward_part ]]; then
+  #     result="$result$forward_part"
+  # elif [[ -n $forward_part ]]; then
+  #     # extra slash removal
+  #     result="${forward_part:1}"
+  # fi
+
+  # echo $result
+}
+
+# function num_jobs()
+# {
+#   local _os=$(os)
+
+# }
+
+
 # \brief Get the openFrameworks name of the host operating system.
 # \param $1 The space-delimited string to de-duplicated and sort.
 # \returns a string an a 0 exit code on success.
