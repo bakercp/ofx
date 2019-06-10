75c03b6924bc1570de56657b4e2fc23fe9948e33
Christopher Baker <me@christopherbaker.net>
2019-06-10 08:17:45 -0600

    add soft link.

--- lib/apothecary_helper.sh
+++ lib/apothecary_helper.sh
@@ -9,7 +9,7 @@ function clean_apothecary()
     exit 1;
   fi
 
-  echoFancy "Building " "${THIS_ADDON_NAME} libraries for ${TARGET_PLATFORM}"
+  echoFancy "Cleaning " "${THIS_ADDON_NAME} libraries for ${TARGET_PLATFORM}"
   /usr/bin/env bash ${OF_APOTHECARY_PATH}/apothecary/apothecary -j ${JOBS} -t "${TARGET_PLATFORM}" -d "${THIS_ADDON_PATH}/libs" clean "${THIS_ADDON_NAME}"
   echoSuccess "Cleaning of ${THIS_ADDON_NAME} for ${TARGET_PLATFORM} complete."
 }
