4e1c5f4f5fcc7f7cb192dd49fc33a82b6a1537cc
Christopher Baker <me@christopherbaker.net>
2019-06-09 22:36:49 -0600

    Fix test.

--- lib/openframeworks_helper.sh
+++ lib/openframeworks_helper.sh
@@ -4,14 +4,14 @@
 
 function install_openframeworks()
 {
-  # if [ -z $1 ] && [ -d $1 ]; then 
+  if [ -d $1 ]; then 
     pushd $1 > /dev/null
     git clone --depth=${OF_CLONE_DEPTH} --branch=${OF_CLONE_BRANCH} https://github.com/${OF_CLONE_USERNAME}/openFrameworks
     popd > /dev/null
     exit 0
-  # else 
-  #   echoError "openFrameworks install directory does not exist: \"$1\""
-  #   exit 1
-  # fi
+  else 
+    echoError "openFrameworks install directory does not exist: \"$1\""
+    exit 1
+  fi
 }
 
