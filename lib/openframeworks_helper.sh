d665ad393f6f0c63772da5853c9e235e4f7309ce
Christopher Baker <me@christopherbaker.net>
2019-06-10 17:15:20 -0600

    Update ofx.

--- lib/openframeworks_helper.sh
+++ lib/openframeworks_helper.sh
@@ -5,6 +5,10 @@
 function install_openframeworks()
 {
     echoFancy "Installing" "openFrameworks"
-    git clone --depth=${OF_CLONE_DEPTH} --branch=${OF_CLONE_BRANCH} https://github.com/${OF_CLONE_USERNAME}/openFrameworks.git ${OF_ROOT}
+    if [ ! -d ${OF_ROOT} ]; then
+        git clone --depth=${OF_CLONE_DEPTH} --branch=${OF_CLONE_BRANCH} https://github.com/${OF_CLONE_USERNAME}/openFrameworks.git ${OF_ROOT}
+    else 
+        echoWarning "${OF_ROOT} already exists"
+    fi
 }
 
