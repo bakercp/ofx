aa5272d1bdbeb0953b16650f3d8c3675bacf9cf0
Christopher Baker <me@christopherbaker.net>
2019-10-12 19:30:30 -0600

    Generic library installation.

--- lib/dependency_helper.sh
+++ lib/dependency_helper.sh
@@ -6,10 +6,13 @@
 
 function install_libraries()
 {
+  THE_ADDON_NAME=$1
+  THE_ADDON_PATH=$2
+
   echoInfo "Installing" "Libraries and System Dependencies"
 
-  if [ -f ${THIS_ADDON_PATH}/scripts/${TARGET_PLATFORM}/install.sh ] ; then
-    /usr/bin/env bash ${THIS_ADDON_PATH}/scripts/${TARGET_PLATFORM}/install.sh
+  if [ -f ${THE_ADDON_PATH}/scripts/${TARGET_PLATFORM}/install.sh ] ; then
+    /usr/bin/env bash ${THE_ADDON_PATH}/scripts/${TARGET_PLATFORM}/install.sh
   fi
 
   echoInfo "Installing" "3rd Party Libraries"
@@ -19,9 +22,9 @@ function install_libraries()
     exit 1;
   fi
 
-  echoInfo "Building " "${THIS_ADDON_NAME} libraries for ${TARGET_PLATFORM}"
-  /usr/bin/env bash ${OF_APOTHECARY_PATH}/apothecary/apothecary -j${JOBS} -t "${TARGET_PLATFORM}" -d "${THIS_ADDON_PATH}/libs" update "${THIS_ADDON_NAME}"
-  echoSuccess "Build of ${THIS_ADDON_NAME} for ${TARGET_PLATFORM} complete."
+  echoInfo "Building " "${THE_ADDON_NAME} libraries for ${TARGET_PLATFORM}"
+  /usr/bin/env bash ${OF_APOTHECARY_PATH}/apothecary/apothecary -j${JOBS} -t "${TARGET_PLATFORM}" -d "${THE_ADDON_PATH}/libs" update "${THE_ADDON_NAME}"
+  echoSuccess "Build of ${THE_ADDON_NAME} for ${TARGET_PLATFORM} complete."
 }
 
 
